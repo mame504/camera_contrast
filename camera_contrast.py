5938743b7fc4476508b3f5fb09ac5c854b2f1d16
mame504 <40550435+mame504@users.noreply.github.com>
2018-07-02 11:00:02 +0900

    Add files via upload

---
+++ camera_contrast.py
@@ -0,0 +1,45 @@
+import numpy as np
+import cv2
+import math
+
+def myfunc(i):
+    pass # do nothing
+
+cv2.namedWindow('title') # create win with win name
+
+cv2.createTrackbar('value', # name of value
+                   'title', # win name
+                   0, # min
+                   100, # max
+                   myfunc) # callback func
+
+
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,  640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+
+while(True):
+
+    ret, frame = cap.read()
+    if not ret: continue
+
+
+    v = cv2.getTrackbarPos('value',  # get the value
+                           'title')  # of the win
+
+    ## do something by using v
+    lut = [ np.uint8(255.0 / (1 + math.exp(-v * (i - 128.) / 255.))) for i in range(256)] 
+    result_image = np.array( [ lut[value] for value in frame.flat], dtype=np.uint8 )
+    result_image = result_image.reshape(frame.shape)
+
+    cv2.imshow('title', result_image)  # show in the win
+
+    k = cv2.waitKey(1)
+    if k == ord('q') or k == 27:
+        break
+
+
+
+cap.release()
+cv2.destroyAllWindows()
